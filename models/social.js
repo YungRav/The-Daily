9d09b56d66050711694f6a2a282b133650f3e687
Carter Keith <carterkeith3gmail.com>
2020-07-11 11:40:36 -0400

    schema2

--- models/social.js
+++ models/social.js
@@ -2,9 +2,16 @@
 
 module.exports = function(sequelize, DataTypes) {
     var Social = sequelize.define("Social", {
-      media1: DataTypes.STRING,
-      media2: DataTypes.STRING
+      facebook: { type: Sequelize.BOOLEAN, defaultValue: 0},
+      twitter: { type: Sequelize.BOOLEAN, defaultValue: 0},
+      reddit: { type: Sequelize.BOOLEAN, defaultValue: 0},
+      instagram: { type: Sequelize.BOOLEAN, defaultValue: 0},
+      youtube: { type: Sequelize.BOOLEAN,defaultValue: 0}
     });
     return Social;
   };
   
+  
+  //need ids to link together since they'll tie back into the user login model
+  //foreign keys
+
