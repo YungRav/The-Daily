df0f8f99b07d0bdaf07c68a4065e74fe2a10de7d
enrightcw <58670012+enrightcw@users.noreply.github.com>
2020-07-11 11:46:55 -0400

    Needed to pull Carter's branch to run lint

--- models/social.js
+++ models/social.js
@@ -1,17 +1,15 @@
 //username and email
 
 module.exports = function(sequelize, DataTypes) {
-    var Social = sequelize.define("Social", {
-      facebook: { type: Sequelize.BOOLEAN, defaultValue: 0},
-      twitter: { type: Sequelize.BOOLEAN, defaultValue: 0},
-      reddit: { type: Sequelize.BOOLEAN, defaultValue: 0},
-      instagram: { type: Sequelize.BOOLEAN, defaultValue: 0},
-      youtube: { type: Sequelize.BOOLEAN,defaultValue: 0}
-    });
-    return Social;
-  };
-  
-  
-  //need ids to link together since they'll tie back into the user login model
-  //foreign keys
+  const Social = sequelize.define("Social", {
+    facebook: { type: DataTypes.BOOLEAN, defaultValue: 0 },
+    twitter: { type: DataTypes.BOOLEAN, defaultValue: 0 },
+    reddit: { type: DataTypes.BOOLEAN, defaultValue: 0 },
+    instagram: { type: DataTypes.BOOLEAN, defaultValue: 0 },
+    youtube: { type: DataTypes.BOOLEAN, defaultValue: 0 }
+  });
+  return Social;
+};
 
+//need ids to link together since they'll tie back into the user login model
+//foreign keys
