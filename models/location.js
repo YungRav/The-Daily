8fa0fd277d88c33394d694aef4a820bc71bed448
enrightcw <58670012+enrightcw@users.noreply.github.com>
2020-07-14 20:18:52 -0400

    Lint fixes again

--- models/location.js
+++ models/location.js
@@ -3,17 +3,15 @@
 module.exports = function(sequelize, DataTypes) {
   const Location = sequelize.define("Location", {
     city: DataTypes.STRING,
-    state: DataTypes.STRING,
-    
+    state: DataTypes.STRING
   });
   Location.associate = function(models) {
-    
     Location.belongsTo(models.User, {
       foreignKey: {
         allowNull: false
       }
     });
   };
-  
+
   return Location;
 };
